2d23c85ecd372b3cfa3ebc08ad69a16d26ca8f84
FelicityTEGHOMO <feliciteteghomo@gmail.com>
2023-05-26 15:33:23 +0100

    Création du contoller ReserverController

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -23,7 +23,7 @@ from odoo.http import request
 class HotelController(http.Controller):
     @http.route('/hotels', auth='public', website=True)
     def hotel_list(self, **kw):
-        hotels = http.request.env['gestion_reservations.hotel'].search([])
+        hotels = http.request.env['gestion_reservations.hotel'].sudo().search([])
         return http.request.render('gestion_reservations.template_hotels', {'hotels': hotels})
 
 # controller chambre
@@ -36,11 +36,32 @@ class ChambreController(http.Controller):
 
 
 # controller réserver
+# class ReserverController(http.Controller):
+#     @http.route('/reserver', auth='public', website=True)
+#     def hotel_list(self, **kw):
+#         reservations = http.request.env['gestion_reservations.reservation'].search([])
+#         return http.request.render('gestion_reservations.template_reservations', {'reservations': reservations})
+
 class ReserverController(http.Controller):
-    @http.route('/reserver', auth='public', website=True)
-    def hotel_list(self, **kw):
-        reservations = http.request.env['gestion_reservations.reservation'].search([])
-        return http.request.render('gestion_reservations.template_reservations', {'reservations': reservations})
+    @http.route('/webform', type="http", auth='public', website=True)
+    def reserver_webform(self,**kw):
+        return http.request.render('gestion_reservations.template_reserver', {})
+
+    @http.route('/create/reserver', type="http", auth='public', website=True)
+    def create_reserver(self,**kw):
+        print("journée")
+        request.env['gestion_reservations.reservation'].sudo().create(kw)
+        reservation_val={
+            'last_name':kw.get('last_name'),
+            'first_name': kw.get('first_name'),
+            'email': kw.get('email'),
+            'adresse':kw.get('adresse'),
+            'ville':kw.get('ville'),
+            'start_date':kw.get('start_date'),
+            'end_date':kw.get('end_date')
+        }
+        request.env['gestion_reservations.reservation'].sudo().create(reservation_val)
+        return request.render('gestion_reservations.reserver_thanks', {})
 
     # @http.route('/gestion_reservation/gestion_reservation/hotels', auth='public')
     # def list(self, **kw):
