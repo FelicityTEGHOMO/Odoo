14a0697f7c0b40aa8e3f19c1fe12f6e224433099
FelicityTEGHOMO <feliciteteghomo@gmail.com>
2023-05-26 15:31:45 +0100

    correspondance entre les éléments du modèle reservation et celui présent sur le site web

--- models/reservation.py
+++ models/reservation.py
@@ -5,15 +5,15 @@ from odoo import models, fields, api
 class Reservation(models.Model):
     _name = 'gestion_reservations.reservation'
     _description = 'reservation'
-    _rec_name = "id_reservation"
+    _rec_name = "email"
 
-    id_reservation = fields.Char(required=True,string="Numéro de la réservation")
-    last_name = fields.Char(required=True, string="Nom")
-    first_name = fields.Char(required=True, string="Prénom")
-    email = fields.Char(required=True, string="Adresse e-mail")
-    adresse = fields.Char(required=True, string="Adresse")
-    ville = fields.Char(required=True,string="Ville")
-    start_date = fields.Date(required=True, string='Date d\'arrivée')
-    end_date = fields.Date(required=True, string='Date de départ')
+    id_reservation = fields.Char(string="Numéro de la réservation")
+    last_name = fields.Char(string="Nom")
+    first_name = fields.Char(string="Prénom")
+    email = fields.Char(string="Adresse e-mail")
+    adresse = fields.Char(string="Adresse")
+    ville = fields.Char(string="Ville")
+    start_date = fields.Date(string='Date d\'arrivée')
+    end_date = fields.Date(string='Date de départ')
     id_customer = fields.Many2one('gestion_reservations.customer', string='Client')
     id_room = fields.Many2one('gestion_reservations.room', string='Chambre')
