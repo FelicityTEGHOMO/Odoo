6ceca32bffa390a6372c0e7f404d780f7a24f3aa
FelicityTEGHOMO <feliciteteghomo@gmail.com>
2023-06-02 04:31:09 +0100

    Suppression de l'attribut required pour l'email

--- models/customer.py
+++ models/customer.py
@@ -7,12 +7,12 @@ class ReservationClient(models.Model):
     _description = 'customer'
     _rec_name = "email"
 
-    id_customer = fields.Char(string="Numéro du client")
+    id_customer = fields.Char(string="Numéro du client", unique=True)
     last_name = fields.Char(required=True, string="Nom du client")
     first_name = fields.Char(required=True, string="Prénom du client")
     country = fields.Char(required=True, string="Pays du client")
     phone = fields.Char(required=True, string="Téléphone du client")
-    email = fields.Char(required=True,string="Email du client")    
+    email = fields.Char(string="Email du client")    
     start_date = fields.Date(string='Date d\'arrivée')
     end_date = fields.Date(string='Date de départ')
     id_room = fields.Many2one('gestion_reservations.room', string='Chambre')
