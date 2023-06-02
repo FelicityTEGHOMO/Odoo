396adfb94bd9144aee5cec1049023fb9ac51e835
FelicityTEGHOMO <feliciteteghomo@gmail.com>
2023-06-02 15:07:49 +0100

    Création de la méthode qui change le statut d'une chambre quand elle est réservée

--- models/reservation.py
+++ models/reservation.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Reservation(models.Model):
     _name = 'gestion_reservations.reservation'
@@ -12,8 +13,31 @@ class Reservation(models.Model):
     first_name = fields.Char(string="Prénom")
     email = fields.Char(string="Adresse e-mail")
     country = fields.Char(string="Pays du client")
-    phone = fields.Char(required=True, string="Téléphone du client")
+    phone = fields.Char( string="Téléphone du client")
     start_date = fields.Date(string='Date d\'arrivée')
     end_date = fields.Date(string='Date de départ')
     id_customer = fields.Many2one('gestion_reservations.customer', string='Client')
     id_room = fields.Many2one('gestion_reservations.room', string='Chambre')
+    cool= fields.Boolean()
+
+
+    """@api.depends('id_room')
+    def _change_state_room(self):
+        if self.id_room:
+            self.id_room.state='occupied'
+            self.cool=True
+        else:
+            self.cool=False """
+
+    
+
+    @api.constrains('id_room')
+    def check_chamber(self):
+        rooms= self.env['gestion_reservations.room'].search([('id','=',self.id_room.id)])
+        if rooms.state=='Disponible':
+            rooms.state='occupied'
+        else:
+            raise UserError('La chambre est déja réservée')
+
+
+    
