6347a9a17e10bde9083e2773a5938874d0300e57
FelicityTEGHOMO <feliciteteghomo@gmail.com>
2023-06-02 04:31:53 +0100

    Mise à jour du modèle hôtel

--- models/hotel.py
+++ models/hotel.py
@@ -7,33 +7,12 @@ class ReservationHotel(models.Model):
     _description = 'hotel'
     _rec_name = "name_hotel"
 
-    id_hotel = fields.Integer(required=True, string="Numéro de l'hôtel")
+    id_hotel = fields.Integer(required=True, string="Numéro de l'hôtel", unique=True)
     name_hotel = fields.Char(required=True, string="Nom de l'hôtel")
     description = fields.Char(required=True, string="Description de l'hôtel")
     tel_hotel = fields.Char(required=True, string="Téléphone de l'hôtel")
     adresse_hotel = fields.Char(required=True, string="Adresse de l'hôtel")
     ville = fields.Char(required=True, string="Ville de l'hôtel")
     image = fields.Image(string="Logo")
-    # id_customer = fields.Many2many('gestion_reservations.customer', 'id_hotel', string='Customer')
     id_room = fields.One2many('gestion_reservations.room', 'id_hotel', string='Chambre')
-    # room_amount = fields.Integer(required=True, string='Prix de la chambre')
-    # start_date = fields.Date(required=True, string='Date d\'arrivée')
-    # end_date = fields.Date(required=True, string='Date de départ')
-
-
-    # @api.model
-    # def create(self, vals):
-    #     image = vals.get('ap_logo')
-    #     if image:
-    #         vals['image'] = image 
-    #     res = super(ReservationHotel, self).create(vals)
-
-# Cette méthode retourne le nombre de chambres pour chaque hotel
-
-    # def get_hotel_room_count(self):
-    #     rooms_data = self.env['gestion_reservations.room'].read_group([('id_hotel', 'in', self.ids)], ['id_hotel'], ['id_hotel'])
-    #     result = dict((data['id_hotel'][0], data['id_hotel_count']) for data in rooms_data)
-    #     for record in self:
-    #         record.hotel_room_count = result.get(record.id, 0)
-
-    # hotel_room_count = fields.Integer(compute='get_hotel_room_count', string='Nombre de chambres')
+  
